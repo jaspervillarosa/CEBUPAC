249524a06c6dbc5e22e5f854a832cd0e5d194bb0
jaspervillarosa <60306260+jaspervillarosa@users.noreply.github.com>
2020-02-21 11:29:38 +0800

    Add files via upload

---
+++ VILLAROSAWK6.js
@@ -0,0 +1,26 @@
+function output(){
+	
+	var LN= document.getElementById("LN").value;
+	var FN=document.getElementById("FN").value;
+	var G= document.getElementById("Gen").value;
+	var EA=document.getElementById("EA").value;
+	var O= document.getElementById("O").value;
+	var D=document.getElementById("D").value;
+	var DD= document.getElementById("DD").value;
+	var RD=document.getElementById("RD").value;
+	
+	if (!LN||!FN||!G||!EA||!O||!D||!DD||!RD)
+	{
+		document.getElementById("display").innerHTML="Please Fill in All the Required Data Entries!";
+	}
+	else if (!EA.includes("@")){
+		document.getElementById("display").innerHTML="Invalid Email!";
+	}
+	else if (DD>=RD){
+		document.getElementById("display").innerHTML="Date Invalid";
+	}
+	else{
+		document.getElementById("display").innerHTML="NAME: "+FN+" "+LN+"<br>Gender:"+G+" <br>Email Address:"+EA+"<br>DESTINATION: "+D+"<br>ORIGIN: "+O+"<br>Departure Date:"+DD+"<br>Return Date:"+RD+"";
+	}
+	
+}
